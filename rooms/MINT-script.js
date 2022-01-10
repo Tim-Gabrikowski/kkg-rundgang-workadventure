ff6619c2c4e5f01c29a3fee6efed76511c3ea1f8
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-01-10 19:25:43 +0100

    Add Chemie2

--- rooms/MINT-script.js
+++ rooms/MINT-script.js
@@ -20,3 +20,13 @@ WA.room.onLeaveLayer("Trigger-Bio").subscribe(() => {
 	WA.room.showLayer("Bio");
 	WA.room.hideLayer("Gang");
 });
+
+//Chemie2 "Dach":
+WA.room.onEnterLayer("Trigger-Chemie2").subscribe(() => {
+	WA.room.hideLayer("Chemie2");
+	WA.room.showLayer("Gang");
+});
+WA.room.onLeaveLayer("Trigger-Chemie2").subscribe(() => {
+	WA.room.showLayer("Chemie2");
+	WA.room.hideLayer("Gang");
+});
