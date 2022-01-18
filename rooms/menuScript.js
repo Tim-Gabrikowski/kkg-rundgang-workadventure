53d4cd260196da68981287f8b0818320c3fe05ec
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-01-18 20:28:44 +0100

    Kunsträume

---
+++ rooms/menuScript.js
@@ -0,0 +1,3 @@
+const menu = WA.ui.registerMenuCommand("menu test", {
+	iframe: "iFrame-Test.html",
+});
--- rooms/testScript.js
+++ rooms/testScript.js
@@ -8,24 +8,28 @@ WA.room.onEnterLayer("stageTrigger").subscribe(() => {
 });
 
 WA.onInit().then(() => {
-		
 	// Popup öffenen, wenn man den Badge in der Aula findet ...
 	badgeAulaPopup = WA.room.onEnterLayer("badgeAula").subscribe(() => {
 		WA.controls.disablePlayerControls();
-		WA.ui.openPopup("badge2", 'ein(e) Buchstabe/Zahl des Lösungswortes: T', [{
-			label: "schliessen",
-			className: "primary",
-			callback: (popup) => {
-				WA.controls.restorePlayerControls();
-				// Schliesse popup, wenn 'schliessen' Button gedrückt wird"
-				popup.close();
-			}
-		}]);
+		WA.ui.openPopup(
+			"badge2",
+			"ein(e) Buchstabe/Zahl des Lösungswortes: T",
+			[
+				{
+					label: "schliessen",
+					className: "primary",
+					callback: (popup) => {
+						WA.controls.restorePlayerControls();
+						// Schliesse popup, wenn 'schliessen' Button gedrückt wird"
+						popup.close();
+					},
+				},
+			]
+		);
 	});
 
 	// Schließe Popup, wenn man map verlässt.
 	WA.room.onLeaveLayer("badgeAula").subscribe(() => {
 		badgeAulaPopup.close();
 	});
-		
 });
