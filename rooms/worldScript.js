f589b7bbcc060b87be058e146bc27b20aabf2b6b
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-01-21 15:48:41 +0100

    Karte einfügen

--- rooms/worldScript.js
+++ rooms/worldScript.js
@@ -1,16 +1,14 @@
 /* Badges */
-function popup(code, idx)
-{
+function popup(code, idx) {
 	WA.controls.disablePlayerControls();
 	WA.ui.openPopup(
 		"badge" + (idx + 1),
-		(1 + idx) + ". Buchstabe/Zahl des Lösungswortes: " + code[idx],
+		1 + idx + ". Buchstabe/Zahl des Lösungswortes: " + code[idx],
 		[
 			{
 				label: "Schliessen",
 				className: "primary",
-				callback: (popup) =>
-				{
+				callback: (popup) => {
 					WA.controls.restorePlayerControls();
 					popup.close();
 				},
@@ -19,95 +17,115 @@ function popup(code, idx)
 	);
 }
 
-function badge(idx)
-{
+function badge(idx) {
 	var req = new XMLHttpRequest();
-	req.addEventListener("load", function()
-	{
+	req.addEventListener("load", function () {
 		popup(this.responseText, idx);
 	});
 
-	req.open("GET", "https://antoninformatik.net/badges/get/?name=" + WA.player.name);
+	req.open(
+		"GET",
+		"https://antoninformatik.net/badges/get/?name=" + WA.player.name
+	);
 	req.send();
 }
 
 /* Badge Eingang */
-let badgeEingangPopup = WA.room.onEnterLayer("badgeEingang").subscribe(() => { badge(0); });
+let badgeEingangPopup = WA.room.onEnterLayer("badgeEingang").subscribe(() => {
+	badge(0);
+});
 
 /* Badge Aula */
-let badgeAulaPopup = WA.room.onEnterLayer("badgeAula").subscribe(() => { badge(1); });
+let badgeAulaPopup = WA.room.onEnterLayer("badgeAula").subscribe(() => {
+	badge(1);
+});
 
 /* Badge Physik */
-let physikBadgePopup = WA.room.onEnterLayer("easterEggs/badgePhysik").subscribe(() => { badge(2); });
+let physikBadgePopup = WA.room
+	.onEnterLayer("easterEggs/badgePhysik")
+	.subscribe(() => {
+		badge(2);
+	});
 
 /* Badge Chemie */
-let chemieBadgePopup = WA.room.onEnterLayer("easterEggs/badgeChemie").subscribe(() => { badge(3); });
+let chemieBadgePopup = WA.room
+	.onEnterLayer("easterEggs/badgeChemie")
+	.subscribe(() => {
+		badge(3);
+	});
 
 /* Badge Bibliothek */
-let badgeBibliothekPopup = WA.room.onEnterLayer("badgeBibliothek").subscribe(() => { badge(4); });
+let badgeBibliothekPopup = WA.room
+	.onEnterLayer("badgeBibliothek")
+	.subscribe(() => {
+		badge(4);
+	});
 
 /* Badge Informatik */
-let badgeInformatikPopup = WA.room.onEnterLayer("badgeInformatik").subscribe(() => { badge(5); });
+let badgeInformatikPopup = WA.room
+	.onEnterLayer("badgeInformatik")
+	.subscribe(() => {
+		badge(5);
+	});
 
 /* Badge Klassenraum */
-let badgeKlassenraumPopup = WA.room.onEnterLayer("badgeKlassenraum").subscribe(() => { badge(6); });
+let badgeKlassenraumPopup = WA.room
+	.onEnterLayer("badgeKlassenraum")
+	.subscribe(() => {
+		badge(6);
+	});
 
 /* Eingang Popup */
-let eingangsPopup = WA.room.onEnterLayer("eingang").subscribe(() =>
-{
+let eingangsPopup = WA.room.onEnterLayer("eingang").subscribe(() => {
 	WA.controls.disablePlayerControls();
-	WA.ui.openPopup("popupStart", 'Herzlich Willkommen!',
-	[
+	WA.ui.openPopup("popupStart", "Herzlich Willkommen!", [
 		{
 			label: "schliessen",
 			className: "primary",
-			callback: (popup) =>
-			{
+			callback: (popup) => {
 				WA.controls.restorePlayerControls();
 				popup.close();
-			}
-		}
+			},
+		},
 	]);
 });
 
 /* Intro */
-const menu = WA.ui.registerMenuCommand("Anleitung",
-{
+const menu = WA.ui.registerMenuCommand("Anleitung", {
 	iframe: "../intro.html",
+});
+const menu = WA.ui.registerMenuCommand("Karte", {
+	iframe: "../map.html",
 });
 
 /* Welcome on Stage */
-WA.room.onEnterLayer("stageTrigger").subscribe(() =>
-{
+WA.room.onEnterLayer("stageTrigger").subscribe(() => {
 	WA.chat.sendChatMessage(`Welcome On Stage, ${WA.player.name}! `, "GSSM");
 });
 
 /* Easter Egg: Tim's Stuhl */
-let EasterEggStuhl = WA.room.onEnterLayer("easterEggs/Stuhl-EasterEgg").subscribe(() =>
-{
-	WA.controls.disablePlayerControls();
-	WA.ui.openPopup("meinStuhl", `Tim: Das ist mein Platz!`,
-	[
-		{
-			label: "Close",
-			className: "primary",
-			callback: (popup) =>
+let EasterEggStuhl = WA.room
+	.onEnterLayer("easterEggs/Stuhl-EasterEgg")
+	.subscribe(() => {
+		WA.controls.disablePlayerControls();
+		WA.ui.openPopup("meinStuhl", `Tim: Das ist mein Platz!`, [
 			{
-				WA.controls.restorePlayerControls();
-				popup.close();
-			}
-		}
-	]);
-});
+				label: "Close",
+				className: "primary",
+				callback: (popup) => {
+					WA.controls.restorePlayerControls();
+					popup.close();
+				},
+			},
+		]);
+	});
 
 /* Easter Egg: Lehrersprüche */
 let indexOfSpruch = 0;
 
-function pickSpruch()
-{
+function pickSpruch() {
 	let spruch = sprueche[indexOfSpruch];
-	if(++indexOfSpruch >= sprueche.length)
-	{
+	if (++indexOfSpruch >= sprueche.length) {
 		sprueche.sort(() => Math.random() - 0.5);
 		indexOfSpruch = 0;
 	}
@@ -115,36 +133,36 @@ function pickSpruch()
 	return `Lehrer-Spruch #${spruch.no}:\n${spruch.text}`;
 }
 
-let EasterEggSprueche = WA.room.onEnterLayer("easterEggs/Physik-Trigger").subscribe(() =>
-{
-	WA.controls.disablePlayerControls();
-	WA.ui.openPopup("sprueche", `${pickSpruch()}`,
-	[
-		{
-			label: "Close",
-			className: "primary",
-			callback: (popup) =>
+let EasterEggSprueche = WA.room
+	.onEnterLayer("easterEggs/Physik-Trigger")
+	.subscribe(() => {
+		WA.controls.disablePlayerControls();
+		WA.ui.openPopup("sprueche", `${pickSpruch()}`, [
 			{
-				WA.controls.restorePlayerControls();
-				popup.close();
-			}
-		}
-	]);
-});
+				label: "Close",
+				className: "primary",
+				callback: (popup) => {
+					WA.controls.restorePlayerControls();
+					popup.close();
+				},
+			},
+		]);
+	});
 
 var sprueche = [];
 
 var req = new XMLHttpRequest();
-req.addEventListener("load", function()
-{
+req.addEventListener("load", function () {
 	sprueche = JSON.parse(this.responseText);
-	for(let i = 0; i < sprueche.length; ++i)
-	{
+	for (let i = 0; i < sprueche.length; ++i) {
 		sprueche[i].no = i + 1;
 	}
-	
+
 	sprueche.sort(() => Math.random() - 0.5);
 });
 
-req.open("GET", "https://tim-gabrikowski.github.io/kkg-rundgang-workadventure/rooms/sprueche.json");
+req.open(
+	"GET",
+	"https://tim-gabrikowski.github.io/kkg-rundgang-workadventure/rooms/sprueche.json"
+);
 req.send();
